d90821d77ffe9bcb58bccfb38f2109a8ccd90eec
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2022-11-06 12:40:14 +0100

    header and footer don

--- src/components/home/Footer/FooterBottom.js
+++ src/components/home/Footer/FooterBottom.js
@@ -1,1 +1,24 @@
+import React from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
+
+const FooterBottom = () => {
+  return (
+    <div className="w-full bg-black group">
+      <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
+        <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-white text-xl duration-200 text-sm">
+          <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
+            <AiOutlineCopyright />
+          </span>
+          Copyright 2022 | Gha shopping
+            <span className="ml-1 font-medium group-hover:text-primeColor">
+              
+            </span>
+          
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBottom;
 
